6b6e5469d9696cd10793bdd3b48a094c1cda53c2
Yumnam Arunkumar Singh <arunkumar.singh@maersk.com>
2022-10-11 14:02:10 +0100

    Password 4th rule - At least one lower case.

---
+++ PasswordVerification/Password.Rule.Tests/Rules/OneLowerCaseRuleTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Password.Rule.Rules.Tests
+{
+    public class OneLowerCaseRuleests
+    {
+        [Fact]
+        public void Constructor_Should_SetOrderAndMessage()
+        {
+            // Arrange
+
+            // Act
+            var sut = new OneLowerCaseRule(1, "Password should contain at least one lower case.");
+
+            // Assert
+            sut.Order.Should().Be(1);
+            sut.RuleMessage.Should().Be("Password should contain at least one lower case.");
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("ABC", false)]
+        [InlineData("12345678", false)]
+        [InlineData("123456789", false)]
+        [InlineData("ABCDEFGH", false)]
+        [InlineData("ABCDEFGHI", false)]
+        [InlineData("Aa", true)]
+        [InlineData("abce", true)]
+        public void IsValid_Should_BeAsExpected(string value, bool expectedResult)
+        {
+            // Arrange
+            var password = new Core.Password(value);
+
+            // Act
+            var sut = new OneLowerCaseRule(1, "Password should contain at least one lower case.");
+            var result = sut.IsValid(password);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ PasswordVerification/Password.Rule/Rules/OneLowerCaseRule.cs
@@ -0,0 +1,21 @@
+﻿using Password.Core.Rule;
+
+namespace Password.Rule.Rules
+{
+    public class OneLowerCaseRule : IPasswordRule
+    {
+        public int Order { get; }
+
+        public string RuleMessage { get; }
+        public OneLowerCaseRule(int order, string ruleMessage)
+        {
+            this.Order = order;
+            this.RuleMessage = ruleMessage;
+        }
+
+        public bool IsValid(Core.Password password)
+        {
+            return password.Evaluate(s => (s != null) && s.Any(char.IsLower));
+        }
+    }
+}
