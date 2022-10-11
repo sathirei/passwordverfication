9c29098e761959c813b97ae469c369f2b01f6333
Yumnam Arunkumar Singh <arunkumar.singh@maersk.com>
2022-10-11 19:18:17 +0100

    Add Scenario Test and console app to run the logic

---
+++ PasswordVerification/Password.App/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Password.Engine;
+using Password.Rule.Rules;
+using Password.RuleSet;
+
+object _messageLock = new object();
+
+var ruleEngine = new RuleEngine();
+ConfigureRuleEngine(ruleEngine);
+
+do
+{
+    Console.WriteLine("Please enter your password:"); // For simplicity we are printing the password back to the console however we can mask it
+    var password = Console.ReadLine();
+    var result = ruleEngine.Evaluate(new Password.Core.Password(password));
+    Console.WriteLine();
+    WriteVerificationResult(_messageLock, result);
+} while (true);
+
+void ConfigureRuleEngine(RuleEngine ruleEngine)
+{
+    AddRules(ruleEngine);
+    AddRuleSets(ruleEngine);
+}
+
+void AddRules(RuleEngine ruleEngine)
+{
+    var moreThanEightCharactersRule = new MoreThanEightCharactersRule(1, 1, "Password should be more than 8 characters in length.");
+    var notNullRule = new NotNullRule(2, 2, "Password should be provided.");
+    var oneDigitRule = new OneDigitRule(3, 3, "Password should contain at least one digit.");
+    var oneLowerCaseRule = new OneLowerCaseRule(4, 4, "Password should contain at least one lower case.");
+    var oneUpperCaseRule = new OneUpperCaseRule(5, 5, "Password should contain at least one lower case.");
+
+    ruleEngine.AddRule(moreThanEightCharactersRule)
+        .AddRule(notNullRule)
+        .AddRule(oneDigitRule)
+        .AddRule(oneLowerCaseRule)
+        .AddRule(oneUpperCaseRule);
+}
+
+void AddRuleSets(RuleEngine ruleEngine)
+{
+    var atleastThreeRuleSet = new AtLeastNRuleSet(1, 3);
+    var mandatoryRuleSet = new MandatoryRuleSet(2, new List<int> { 1, 2, 3, 4, 5 });
+
+    ruleEngine.AddRuleSet(atleastThreeRuleSet)
+        .AddRuleSet(mandatoryRuleSet);
+}
+
+void WriteColoredMessage(string message, bool result)
+{
+    lock (_messageLock)
+    {
+        Console.ForegroundColor = result ? ConsoleColor.Green : ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}
+
+void WriteVerificationResult(object _messageLock, PasswordVerificationResult? result)
+{
+    WriteColoredMessage(result.IsVerifiedOkay ? "OK" : "Verification Failed", result.IsVerifiedOkay);
+    Console.WriteLine();
+    Console.WriteLine("Rules:");
+    result?.RuleResults?.OrderBy(x => x.Id).ToList().ForEach(x => WriteColoredMessage($"{x.Id}. {x.Message}", x.IsPass));
+    Console.WriteLine();
+    Console.WriteLine("RuleSets:");
+    result?.RuleSetResults?.OrderBy(x => x.Id).ToList().ForEach(x => WriteColoredMessage($"{x.Id}. {x.Message}", x.IsPass));
+    Console.WriteLine();
+}
---
+++ PasswordVerification/Password.Engine.Tests/ScenarioTests.cs
@@ -0,0 +1,171 @@
+﻿using FluentAssertions;
+using Password.Rule.Rules;
+using Password.RuleSet;
+using Xunit;
+
+namespace Password.Engine.Tests
+{
+    public class ScenarioTests
+    {
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("12345678", false)]
+        [InlineData("123456789", true)]
+        public void MoreThanEightCharactersRule_ResultShouldBeAsExpected(string value, bool expectedResult)
+        {
+            // Arrange
+            var ruleEngine = new RuleEngine();
+            ruleEngine.AddRule(new MoreThanEightCharactersRule(1, 1, "Password should be more than 8 characters in length."));
+
+            // Act
+            var result = ruleEngine.Evaluate(new Core.Password(value));
+
+            // Assert
+            result.IsVerifiedOkay.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", true)]
+        [InlineData("abcdefghi", true)]
+        public void NotNullRule_ResultShouldBeAsExpected(string value, bool expectedResult)
+        {
+            // Arrange
+            var ruleEngine = new RuleEngine();
+            ruleEngine.AddRule(new NotNullRule(1, 1, "Password should be provided."));
+
+            // Act
+            var result = ruleEngine.Evaluate(new Core.Password(value));
+
+            // Assert
+            result.IsVerifiedOkay.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("abc", false)]
+        [InlineData("123456789", true)]
+        public void OneDigitRule_ResultShouldBeAsExpected(string value, bool expectedResult)
+        {
+            // Arrange
+            var ruleEngine = new RuleEngine();
+            ruleEngine.AddRule(new OneDigitRule(1, 1, "Password should contain at least one digit."));
+
+            // Act
+            var result = ruleEngine.Evaluate(new Core.Password(value));
+
+            // Assert
+            result.IsVerifiedOkay.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("ABC", false)]
+        [InlineData("123456789", false)]
+        [InlineData("Aa", true)]
+        [InlineData("abce", true)]
+        public void OneLowerCaseRule_ResultShouldBeAsExpected(string value, bool expectedResult)
+        {
+            // Arrange
+            var ruleEngine = new RuleEngine();
+            ruleEngine.AddRule(new OneLowerCaseRule(1, 1, "Password should contain at least one lower case."));
+
+            // Act
+            var result = ruleEngine.Evaluate(new Core.Password(value));
+
+            // Assert
+            result.IsVerifiedOkay.Should().Be(expectedResult);
+        }
+
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("abc", false)]
+        [InlineData("123456789", false)]
+        [InlineData("aBcdefghi", true)]
+        [InlineData("A", true)]
+        public void OneUpperCaseRule_ResultShouldBeAsExpected(string value, bool expectedResult)
+        {
+            // Arrange
+            var ruleEngine = new RuleEngine();
+            ruleEngine.AddRule(new OneUpperCaseRule(1, 1, "Password should contain at least one uppper case."));
+
+            // Act
+            var result = ruleEngine.Evaluate(new Core.Password(value));
+
+            // Assert
+            result.IsVerifiedOkay.Should().Be(expectedResult);
+        }
+
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("abc1", true)]
+        [InlineData("123456789", true)]
+        [InlineData("aBcdefghi9", true)]
+        public void AtLeastNRuleSet_ResultShouldBeAsExpected_WhenNIsSetTo3(string value, bool expectedResult)
+        {
+            // Arrange
+            var moreThanEightCharactersRule = new MoreThanEightCharactersRule(1, 1, "Password should be more than 8 characters in length.");
+            var notNullRule = new NotNullRule(2, 2, "Password should be provided.");
+            var oneDigitRule = new OneDigitRule(3, 3, "Password should contain at least one digit.");
+            var oneLowerCaseRule = new OneLowerCaseRule(4, 4, "Password should contain at least one lower case.");
+            var oneUpperCaseRule = new OneUpperCaseRule(5, 5, "Password should contain at least one lower case.");
+
+            var atleastThreeRuleSet = new AtLeastNRuleSet(1, 3);
+
+            var ruleEngine = new RuleEngine();
+            ruleEngine.AddRule(moreThanEightCharactersRule)
+                .AddRule(notNullRule)
+                .AddRule(oneDigitRule)
+                .AddRule(oneLowerCaseRule)
+                .AddRule(oneUpperCaseRule)
+                .AddRuleSet(atleastThreeRuleSet);
+
+            // Act
+            var result = ruleEngine.Evaluate(new Core.Password(value));
+
+            // Assert
+            result.IsVerifiedOkay.Should().Be(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("abc1", false)]
+        [InlineData("123456789", false)]
+        [InlineData("aBcdefghi9", true)]
+        public void AtLeastThreeAndOneMandatoryRuleSets_ResultShouldBeAsExpected(string value, bool expectedResult)
+        {
+            // Arrange
+            var moreThanEightCharactersRule = new MoreThanEightCharactersRule(1, 1, "Password should be more than 8 characters in length.");
+            var notNullRule = new NotNullRule(2, 2, "Password should be provided.");
+            var oneDigitRule = new OneDigitRule(3, 3, "Password should contain at least one digit.");
+            var oneLowerCaseRule = new OneLowerCaseRule(4, 4, "Password should contain at least one lower case.");
+            var oneUpperCaseRule = new OneUpperCaseRule(5, 5, "Password should contain at least one lower case.");
+
+            var atleastThreeRuleSet = new AtLeastNRuleSet(1, 3);
+            var mandatoryRuleSet = new MandatoryRuleSet(2, new List<int> { 1, 2, 3, 4, 5 });
+
+            var ruleEngine = new RuleEngine();
+            ruleEngine.AddRule(moreThanEightCharactersRule)
+                .AddRule(notNullRule)
+                .AddRule(oneDigitRule)
+                .AddRule(oneLowerCaseRule)
+                .AddRule(oneUpperCaseRule)
+                .AddRuleSet(atleastThreeRuleSet)
+                .AddRuleSet(mandatoryRuleSet);
+
+            // Act
+            var result = ruleEngine.Evaluate(new Core.Password(value));
+
+            // Assert
+            result.IsVerifiedOkay.Should().Be(expectedResult);
+        }
+    }
+}
