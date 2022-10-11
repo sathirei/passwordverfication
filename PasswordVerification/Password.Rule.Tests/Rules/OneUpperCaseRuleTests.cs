ca83310355a3759c9a82e5fb56159af9000cb647
Yumnam Arunkumar Singh <arunkumar.singh@maersk.com>
2022-10-11 13:58:15 +0100

    Password 3rd rule - At least one Upper case.

---
+++ PasswordVerification/Password.Rule.Tests/Rules/OneUpperCaseRuleTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Password.Rule.Rules.Tests
+{
+    public class OneUpperCaseRuleests
+    {
+        [Fact]
+        public void Constructor_Should_SetOrderAndMessage()
+        {
+            // Arrange
+
+            // Act
+            var sut = new OneUpperCaseRule(1, "Password should contain at least one upper case.");
+
+            // Assert
+            sut.Order.Should().Be(1);
+            sut.RuleMessage.Should().Be("Password should contain at least one upper case.");
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("abc", false)]
+        [InlineData("12345678", false)]
+        [InlineData("123456789", false)]
+        [InlineData("abcdefghi", false)]
+        [InlineData("aBcdefghi", true)]
+        [InlineData("A", true)]
+        public void IsValid_Should_BeAsExpected(string value, bool expectedResult)
+        {
+            // Arrange
+            var password = new Core.Password(value);
+
+            // Act
+            var sut = new OneUpperCaseRule(1, "Password should contain at least one upper case.");
+            var result = sut.IsValid(password);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ PasswordVerification/Password.Rule/Rules/OneUpperCaseRule.cs
@@ -0,0 +1,21 @@
+﻿using Password.Core.Rule;
+
+namespace Password.Rule.Rules
+{
+    public class OneUpperCaseRule : IPasswordRule
+    {
+        public int Order { get; }
+
+        public string RuleMessage { get; }
+        public OneUpperCaseRule(int order, string ruleMessage)
+        {
+            this.Order = order;
+            this.RuleMessage = ruleMessage;
+        }
+
+        public bool IsValid(Core.Password password)
+        {
+            return password.Evaluate(s => (s != null) && s.Any(char.IsUpper));
+        }
+    }
+}
