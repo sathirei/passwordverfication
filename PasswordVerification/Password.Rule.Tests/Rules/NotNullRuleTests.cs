9d1fc258d15db74948653d4d3ff292f4eb0f4593
Yumnam Arunkumar Singh <arunkumar.singh@maersk.com>
2022-10-11 13:55:09 +0100

    Password 2nd rule - Not null

---
+++ PasswordVerification/Password.Rule.Tests/Rules/NotNullRuleTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Password.Rule.Rules.Tests
+{
+    public class NotNullRuleTests
+    {
+        [Fact]
+        public void Constructor_Should_SetOrderAndMessage()
+        {
+            // Arrange
+
+            // Act
+            var sut = new NotNullRule(1, "Password should be provided.");
+
+            // Assert
+            sut.Order.Should().Be(1);
+            sut.RuleMessage.Should().Be("Password should be provided.");
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", true)]
+        [InlineData("abc", true)]
+        [InlineData("12345678", true)]
+        [InlineData("123456789", true)]
+        [InlineData("abcdefghi", true)]
+        public void IsValid_Should_BeAsExpected(string value, bool expectedResult)
+        {
+            // Arrange
+            var password = new Core.Password(value);
+
+            // Act
+            var sut = new NotNullRule(1, "Password should be provided.");
+            var result = sut.IsValid(password);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ PasswordVerification/Password.Rule/Rules/NotNullRule.cs
@@ -0,0 +1,21 @@
+﻿using Password.Core.Rule;
+
+namespace Password.Rule.Rules
+{
+    public class NotNullRule : IPasswordRule
+    {
+        public int Order { get; }
+
+        public string RuleMessage { get; }
+        public NotNullRule(int order, string ruleMessage)
+        {
+            this.Order = order;
+            this.RuleMessage = ruleMessage;
+        }
+
+        public bool IsValid(Core.Password password)
+        {
+            return password.Evaluate(s => s != null);
+        }
+    }
+}
