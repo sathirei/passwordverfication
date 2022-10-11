7337963ce80c5193248da835091ecd7adb493b82
Yumnam Arunkumar Singh <arunkumar.singh@maersk.com>
2022-10-11 14:07:10 +0100

    Password 5th rule - At least one digit.

---
+++ PasswordVerification/Password.Rule.Tests/Rules/OneDigitRuleTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Password.Rule.Rules.Tests
+{
+    public class OneDigitRuleests
+    {
+        [Fact]
+        public void Constructor_Should_SetOrderAndMessage()
+        {
+            // Arrange
+
+            // Act
+            var sut = new OneDigitRule(1, "Password should contain at least one digit.");
+
+            // Assert
+            sut.Order.Should().Be(1);
+            sut.RuleMessage.Should().Be("Password should contain at least one digit.");
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("abc", false)]
+        [InlineData("12345678", true)]
+        [InlineData("123456789", true)]
+        [InlineData("abcdefghi", false)]
+        [InlineData("a9cdefghi", true)]
+        [InlineData("A", false)]
+        public void IsValid_Should_BeAsExpected(string value, bool expectedResult)
+        {
+            // Arrange
+            var password = new Core.Password(value);
+
+            // Act
+            var sut = new OneDigitRule(1, "Password should contain at least one digit.");
+            var result = sut.IsValid(password);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ PasswordVerification/Password.Rule/Rules/OneDigitRule.cs
@@ -0,0 +1,21 @@
+﻿using Password.Core.Rule;
+
+namespace Password.Rule.Rules
+{
+    public class OneDigitRule : IPasswordRule
+    {
+        public int Order { get; }
+
+        public string RuleMessage { get; }
+        public OneDigitRule(int order, string ruleMessage)
+        {
+            this.Order = order;
+            this.RuleMessage = ruleMessage;
+        }
+
+        public bool IsValid(Core.Password password)
+        {
+            return password.Evaluate(s => (s != null) && s.Any(char.IsDigit));
+        }
+    }
+}
