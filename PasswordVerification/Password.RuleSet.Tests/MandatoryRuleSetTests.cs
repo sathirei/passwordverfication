422938c61a60046b54f9a13cf3fbf035668c0d6c
Yumnam Arunkumar Singh <arunkumar.singh@maersk.com>
2022-10-11 15:34:43 +0100

    Password 2nd RuleSet - All Mandatory rules.

---
+++ PasswordVerification/Password.RuleSet.Tests/MandatoryRuleSetTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace Password.RuleSet.Tests
+{
+    public class MandatoryRuleSetTests
+    {
+        [Fact()]
+        public void IsValid_Should_ReturnTrue_WhenAllMandatoryRulesAreValid()
+        {
+            // Arrange
+            var ruleResults = new Dictionary<int, bool> { { 1, true }, { 2, true }, { 3, false }, { 4, true } };
+
+            // Act
+            var sut = new MandatoryRuleSet(ruleSetId: 1, mandatoryRulesId: new List<int> { 1, 4 });
+            var result = sut.IsValid(ruleResults);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact()]
+        public void IsValid_Should_ReturnFalse_WhenSomeMandatoryRulesAreNotValid()
+        {
+            // Arrange
+            var ruleResults = new Dictionary<int, bool> { { 1, true }, { 2, true }, { 3, true }, { 4, false } };
+
+            // Act
+            var sut = new MandatoryRuleSet(ruleSetId: 1, mandatoryRulesId: new List<int> { 1, 4 });
+            var result = sut.IsValid(ruleResults);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact()]
+        public void IsValid_Should_ReturnFalse_WhenAllMandatoryRulesAreNotValid()
+        {
+            // Arrange
+            var ruleResults = new Dictionary<int, bool> { { 1, false }, { 2, true }, { 3, false }, { 4, false } };
+
+            // Act
+            var sut = new MandatoryRuleSet(ruleSetId: 1, mandatoryRulesId: new List<int> { 1, 4 });
+            var result = sut.IsValid(ruleResults);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
---
+++ PasswordVerification/Password.RuleSet/MandatoryRuleSet.cs
@@ -0,0 +1,19 @@
+﻿namespace Password.RuleSet
+{
+    public class MandatoryRuleSet : BasePasswordRuleSet
+    {
+        private readonly List<int> mandatoryRuleIds;
+        public MandatoryRuleSet(int ruleSetId, List<int> mandatoryRulesId) : base(ruleSetId) => mandatoryRuleIds = mandatoryRulesId;
+        public override bool IsValid(IDictionary<int, bool> ruleResults)
+        {
+            foreach (var ruleId in mandatoryRuleIds)
+            {
+                if (!ruleResults.TryGetValue(ruleId, out var ruleResult) || !ruleResult)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
